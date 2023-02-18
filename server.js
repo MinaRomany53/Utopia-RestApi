89a6f108a82a2e9425fe39ccbf22a62a9d75c9c7
MinaRomany53 <minaromany190@gmail.com>
2023-02-18 22:11:11 +0200

    Connect to Hosted DB

--- server.js
+++ server.js
@@ -1,4 +1,20 @@
+const mongoose = require("mongoose");
 const app = require("./app");
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DATABASE_PASSWORD
+);
+
+// 🟢 Connect to Hosted Database from Atlas Cloud
+mongoose.set("strictQuery", true); // strict schemas & store in DB only what specified in the model
+mongoose
+  .connect(DB)
+  .then(() => console.log("Database connection successful ✅"))
+  .catch((err) => {
+    console.error("Database connection error ❌");
+    console.log(err); // Handle This Error Later 🚩🚩🚩🚩
+  });
 
 app.listen(process.env.PORT, () => {
   console.log(` http://localhost:5000/api/v1/users`);
