3c74027343977295d42c0668b5492fad11d98542
MinaRomany53 <minaromany190@gmail.com>
2023-02-19 19:48:09 +0200

    Modeling item Schema

---
+++ Models/itemModel.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+
+// Create Item Schema
+const itemSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  price: { type: String, required: true },
+  description: { type: String, required: true },
+  coverImg: { type: String, required: true },
+  imgs: { type: [String] },
+  category: {
+    enum: {
+      values: [
+        "Vehicles",
+        "Properties",
+        "Electronics",
+        "Furniture",
+        "Books",
+        "Services",
+        "Accessories",
+        "Other",
+      ],
+      message:
+        "{VALUE} is not supported, You must choose category field from: Vehicles - Properties -    Electronics - Furniture - Books - Services - Accessories - Other",
+    },
+    required: true,
+  },
+  address: { type: String, required: true },
+  createAt: { type: Date, default: Date.now },
+  closed: { type: Boolean, default: false },
+
+  //user
+});
+
+// Create Item Model
+const Item = mongoose.model("Item", itemSchema);
+module.exports = Item;
