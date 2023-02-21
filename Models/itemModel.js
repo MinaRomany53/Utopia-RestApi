cc74b6b1e6b3234078a39a4eff2cbf8bdaea59a8
MinaRomany53 <minaromany190@gmail.com>
2023-02-21 19:39:23 +0200

    Update Item Schema add some mongoose middleware

--- Models/itemModel.js
+++ Models/itemModel.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
 // Create Item Schema
 const itemSchema = new mongoose.Schema({
@@ -28,9 +29,35 @@ const itemSchema = new mongoose.Schema({
   address: { type: String, required: true, trim: true },
   createAt: { type: Date, default: Date.now },
   closed: { type: Boolean, default: false },
+  slug: { type: String },
 
   //user
 });
+
+/* ------------------- Start Mongoose Middleware ------------------- */
+
+// Document Middleware  -  runs before create() and save()
+itemSchema.pre("save", function (next) {
+  this.slug = slugify(this.title, { lower: true });
+  this.title = this.title.replace(/  +/g, " ");
+  next();
+});
+
+// Query Middleware  - runs before find()
+itemSchema.pre(/^find/, function (next) {
+  this.find({ closed: { $ne: true } }); // not showing any secret tours
+  this.projection({ closed: 0 }); // not showing any secret tours
+  next();
+});
+
+// Aggregation Middleware  - runs before aggregate() only
+itemSchema.pre("aggregate", function (next) {
+  const stage = { $match: { closed: { $ne: true } } };
+  this.pipeline().unshift(stage); // add new stage at the first of aggregation pipeline array
+  next();
+});
+
+/* ------------------- End Mongoose Middleware --------------------- */
 
 // Create Item Model
 const Item = mongoose.model("Item", itemSchema);
