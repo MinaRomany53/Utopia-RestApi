6baeea6a6fb855e9dc95c73fa4215deef2dbe03b
MinaRomany53 <minaromany190@gmail.com>
2023-02-22 01:36:58 +0200

    Error-Handling Middleware

--- app.js
+++ app.js
@@ -26,10 +26,28 @@ app.use("/api/v1/items", itemsRouter);
 
 // Handling Unhandled Routes
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "Fail",
-    date: req.date,
-    message: `Can't find ${req.originalUrl} in this server!`,
+  // res.status(404).json({
+  //   status: "Fail",
+  //   date: req.date,
+  //   message: `Can't find ${req.originalUrl} in this server!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} in this server!`);
+  err.statusCode = 404;
+  err.status = "Fail";
+  next(err);
+});
+
+// Error-Handling Middleware - Handling All Errors in the App
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+  err.message = err.message || "Something Wrong happen";
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
   });
 });
 
